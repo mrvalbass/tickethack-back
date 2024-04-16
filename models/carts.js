459a2316a60e61a01988fbbea38e8e7c63b33240
Florent <florent.degeorges@gmail.com>
2024-04-16 16:24:51 +0200

    Creation completed routes for cart get, post, deleteOne and deleteAll

--- models/carts.js
+++ models/carts.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const cartSchema = mongoose.Schema({
-  departure: String,
-  arrival: String,
-  date: Date,
-  price: Number,
+  trip_id: { type: mongoose.Schema.Types.ObjectId, ref: "trips" },
 });
 
 const Cart = mongoose.model("carts", cartSchema);
--- routes/carts.js
+++ routes/carts.js
@@ -4,11 +4,43 @@ const Cart = require("../models/carts");
 
 /* GET all trips listed in cart. */
 router.get("/", (req, res) => {
-  Cart.find().then((data) => {
-    res.json({ allTripsInCart: data });
-  });
+  Cart.find()
+    .populate("trip_id")
+    .then((data) => {
+      res.json({ allTripsInCart: data.map((obj) => obj.trip_id) });
+    });
 });
 
 /* POST add a trip in cart. */
+router.post("/", (req, res) => {
+  const newCart = new Cart({
+    trip_id: req.body.trip_id,
+  });
+  newCart.save().then((data) => {
+    res.json({ trip_id: data });
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  Cart.deleteOne({ trip_id: req.params.id }).then((data) => {
+    console.log(data);
+    if (data.deletedCount > 0) {
+      res.json((result = true));
+    } else {
+      res.json((result = false));
+    }
+  });
+});
 
 module.exports = router;
+
+router.delete("/", (req, res) => {
+  Cart.deleteMany().then((data) => {
+    console.log(data);
+    if (data.deletedCount > 0) {
+      res.json((result = true));
+    } else {
+      res.json((result = false));
+    }
+  });
+});
