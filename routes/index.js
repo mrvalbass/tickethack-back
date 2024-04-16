cf997894907b14737cddfaeb62e774cf48d37009
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-04-16 09:30:06 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+constexpress = require("express");
+constrouter = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
