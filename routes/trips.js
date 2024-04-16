e78e8fc885a7ca9a405b74cbed569e7c55d68bc7
Florent <florent.degeorges@gmail.com>
2024-04-16 13:17:25 +0200

    working on dates

--- routes/trips.js
+++ routes/trips.js
@@ -8,16 +8,29 @@ const Trip = require("../models/trips");
 //   res.send("respond with a resource");
 // });
 
-router.get("/", (req, res) => {
-  Trip.find().then((data) => res.json({ allTrips: data }));
-});
+// router.get("/", (req, res) => {
+//   Trip.find().then((data) => res.json({ allTrips: data }));
+// });
 
 router.get("/", (req, res) => {
+  //   console.log("this is date", new Date(req.query.date).getDate());
+  //   console.log("this is greater than", new Date(`${new Date(req.query.date)}`));
+  //   console.log(
+  //     "this is greater than",
+  //     new Date(new Date(req.query.date) + "T23:59:59.999Z")
+  //   );
+  const today = new Date(req.query.date);
+  const tomorrow = new Date(today.setDate(today.getDate() + 1));
+  console.log("this is tomorrow", tomorrow);
   Trip.find({
-    departure: req.body.departure,
-    arrival: req.body.arrival,
+    departure: req.query.departure,
+    arrival: req.query.arrival,
+    date: {
+      $gt: today,
+      $lt: tomorrow,
+    },
   }).then((data) => {
-    console.log("This is info by destination", data);
+    //console.log("This is info by destination", data);
     res.json({ allTripsByDestination: data });
   });
 });
